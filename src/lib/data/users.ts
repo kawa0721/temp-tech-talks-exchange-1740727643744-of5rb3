86fa72a1acaed885a3ef89070b06149d747610d1
MK <56499013+kawa0721@users.noreply.github.com>
2025-02-28 16:31:28 +0900

    Copy src/lib/data/users.ts from tech-talks-exchange

---
+++ src/lib/data/users.ts
@@ -0,0 +1,30 @@
+
+import { User } from "@/types";
+
+export const USERS: User[] = [
+  {
+    id: 'user-1',
+    name: '田中 太郎',
+    avatar: 'https://i.pravatar.cc/150?img=1',
+  },
+  {
+    id: 'user-2',
+    name: '山田 花子',
+    avatar: 'https://i.pravatar.cc/150?img=2',
+  },
+  {
+    id: 'user-3',
+    name: '佐藤 次郎',
+    avatar: 'https://i.pravatar.cc/150?img=3',
+  },
+  {
+    id: 'user-4',
+    name: '鈴木 さくら',
+    avatar: 'https://i.pravatar.cc/150?img=4',
+  },
+  {
+    id: 'user-5',
+    name: '高橋 五郎',
+    avatar: 'https://i.pravatar.cc/150?img=5',
+  },
+];
